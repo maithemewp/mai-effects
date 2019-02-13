9fa968b4ecd6497ee07f0dfe946e81f7e2d52cf9
Mike Hemberger <mike@thestizmedia.com>
2019-02-13 15:18:10 -0500

    v bump

--- mai-effects.php
+++ mai-effects.php
@@ -4,7 +4,7 @@
  * Plugin Name:     Mai Effects
  * Plugin URI:      https://maitheme.com
  * Description:     Add various section effects to add a little flair to your Mai Theme powered website.
- * Version:         0.7.0
+ * Version:         0.8.0
  *
  * Author:          MaiTheme.com
  * Author URI:      https://maitheme.com
@@ -90,7 +90,7 @@ final class Mai_Effects {
 
 		// Plugin version.
 		if ( ! defined( 'MAI_EFFECTS_VERSION' ) ) {
-			define( 'MAI_EFFECTS_VERSION', '0.7.0' );
+			define( 'MAI_EFFECTS_VERSION', '0.8.0' );
 		}
 
 		// Plugin Folder Path.
