f17191cf6c8b13c2a4c70a007e8d290f9b39ba83
Mike Hemberger <mike@thestizmedia.com>
2019-03-29 10:25:14 -0400

    0.9.3 - wait till dom loaded

--- assets/js/mai-effects.js
+++ assets/js/mai-effects.js
@@ -1,57 +1,62 @@
-// Parallax.
-document.querySelectorAll( '.parallax' ).forEach( function(element) {
+// Wait till content is loaded.
+document.addEventListener( 'DOMContentLoaded', function(event) {
 
-	var image = element.querySelector( '.bg-image' );
+	// Parallax.
+	document.querySelectorAll( '.parallax' ).forEach( function(element) {
 
-	// Skip if no image.
-	if ( ! image ) {
-		return;
-	}
+		var image = element.querySelector( '.bg-image' );
 
-	var parallaxScroll = basicScroll.create({
-		elem: element,
-		from: 'top-bottom',
-		to: 'bottom-top',
-		direct: image,
-		props: {
-			'--translateY': {
-				from: '-20%',
-				to: '20%',
-			}
-		},
-	})
-	.start();
+		// Skip if no image.
+		if ( ! image ) {
+			return;
+		}
 
-});
+		var parallaxScroll = basicScroll.create({
+			elem: element,
+			from: 'top-bottom',
+			to: 'bottom-top',
+			direct: image,
+			props: {
+				'--translateY': {
+					from: '-20%',
+					to: '20%',
+				}
+			},
+		})
+		.start();
 
-// Fades.
-document.querySelectorAll( '.has-fadein, .has-fadeinup, .has-fadeindown, .has-fadeinleft, .has-fadeinright' ).forEach( function(element) {
+	});
 
-	var fadeClassAdded = false;
+	// Fades.
+	document.querySelectorAll( '.has-fadein, .has-fadeinup, .has-fadeindown, .has-fadeinleft, .has-fadeinright' ).forEach( function(element) {
 
-	var fadeScroll = basicScroll.create({
-		elem: element,
-		from: 'top-bottom',
-		to: 'top-top',
-		direct: true,
-		inside: (instance, percentage, props) => {
-			if ( fadeClassAdded ) {
-				return;
-			}
-			if ( percentage <= 20 ) {
-				return;
-			}
-			element.classList.add( 'doFade' );
-			fadeClassAdded = true;
-		},
-		outside: (instance, percentage, props) => {
-			// Fade in if loading the page after it's already scrolled past.
-			if ( ( percentage >= 100 ) && ! fadeClassAdded ) {
+		var fadeClassAdded = false;
+
+		var fadeScroll = basicScroll.create({
+			elem: element,
+			from: 'top-bottom',
+			to: 'top-top',
+			direct: true,
+			inside: (instance, percentage, props) => {
+				if ( fadeClassAdded ) {
+					return;
+				}
+				if ( percentage <= 20 ) {
+					return;
+				}
 				element.classList.add( 'doFade' );
 				fadeClassAdded = true;
+			},
+			outside: (instance, percentage, props) => {
+				// Fade in if loading the page after it's already scrolled past.
+				if ( ( percentage >= 100 ) && ! fadeClassAdded ) {
+					element.classList.add( 'doFade' );
+					fadeClassAdded = true;
+				}
 			}
-		}
-	})
-	.start();
+		})
+		.start();
+
+	});
 
 });
--- mai-effects.php
+++ mai-effects.php
@@ -4,7 +4,7 @@
  * Plugin Name:     Mai Effects
  * Plugin URI:      https://maitheme.com
  * Description:     Add a little flair to your Mai Theme powered website.
- * Version:         0.9.2
+ * Version:         0.9.3
  *
  * Author:          MaiTheme.com
  * Author URI:      https://maitheme.com
@@ -90,7 +90,7 @@ final class Mai_Effects {
 
 		// Plugin version.
 		if ( ! defined( 'MAI_EFFECTS_VERSION' ) ) {
-			define( 'MAI_EFFECTS_VERSION', '0.9.2' );
+			define( 'MAI_EFFECTS_VERSION', '0.9.3' );
 		}
 
 		// Plugin Folder Path.
